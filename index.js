317c551d5f6f4a926a1e2df0686b2aa93c832cfa
ningrong <ningrong@minieye.cc>
2021-05-05 16:38:41 +0800

    chore: add reprFunction / reprObject / parseValue / reprJobProperty /manageIfMain ...

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const child_process = require("child_process");
 
-function bool_val(v) {
+function bool_val(v, false_alias) {
     if (!v) {
         return false;
     }
-    const ns = ["", "0", "false", "no", "n", "f"];
+    const ns = ["", "0", "false", "[]", "{}", "null", "undefined"];
+    if (false_alias instanceof Array) {
+        ns.push(...false_alias);
+    }
     if (typeof (v) === "string") {
         v = v.trim().toLowerCase();
         if (ns.indexOf(v) > -1) {
@@ -22,7 +25,7 @@ const self = module.exports = {
         DEBUG: bool_val(process.env.DEBUG),
         LOG_LEVEL: process.env.LOG_LEVEL || "info",       // default:info ;  debug, info, warn, error, trace
         LOG_DEPTH: parseInt(process.env.LOG_DEPTH) || 0,  // default: 0   ;  loop depth of stringify object
-        DISABLE_LOOP: bool_val(process.env.DISABLE_LOOP), // default:false ; run sub-job
+        DISABLE_SEEK: bool_val(process.env.DISABLE_SEEK), // default:false ; run sub-job
     },
     _log: (...args) => {
         let calv = self.config.LOG_LEVEL.toLowerCase();
@@ -39,7 +42,65 @@ const self = module.exports = {
         self._log(`//spend ${t2.valueOf() - t1.valueOf()} ms, finish@${t2.toJSON()}, start@${t1.toJSON()}`);
     },
 
+    reprFunction: (func) => {
+        // represent function as a string
+        // typeof (func) === "function"
+        let pf = String(func);
+        if (pf.startsWith("function ")) {
+            let r = new RegExp("\\)[\\s]*\\{");
+            let repr = pf.split(r, 1)[0];
+            return `${repr})`;
+        } else {
+            let repr = pf.split("=>", 1)[0];
+            return `${repr}`;
+        }
+    },
+
+    reprObject: (obj) => {
+        // represent object as a string
+        // typeof (obj) === "object"
+        let v = undefined;
+        try {
+            v = JSON.stringify(obj, null, 2);
+        } catch (e) {
+            self._log(e);
+        }
+        if (v === undefined && (obj !== undefined)) {
+            v = `${obj.__proto__.constructor.name}: ${obj}`;
+        }
+        return v;
+    },
+
+    parseValue: (v) => {
+        let tp = typeof (v);
+        if (tp === "function") {
+            return {is_true: true, repr: self.reprFunction(v), value: v};
+        } else {
+            if (tp === "object" && v !== null) {
+                let repr = self.reprObject(v);
+                return {is_true: bool_val(repr), repr: repr, value: v};
+            } else {
+                return {is_true: Boolean(v), repr: String(v), value: v};
+            }
+        }
+    },
+
+    reprJobProperty: (jobModule, attr) => {
+        let v = jobModule[attr];
+        let lines = [];
+        lines.push(`[${attr}] : `);
+        if (v instanceof Array) {
+            let ns = v.map((e, i) => `\t${self.parseValue(e).repr}`);
+            lines.push(...ns);
+        } else {
+            lines.push(self.parseValue(v).repr);
+        }
+        return lines.join("\n");
+    },
+
     execCommand: async (cmd, exit_if_finish = false) => {
+        // execute commands with shell (Default: '/bin/sh' on Unix, process.env.ComSpec on Windows.)
+        // https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
         const exec = new Promise(function (resolve, reject) {
             let t1 = new Date();
             child_process.exec(cmd, (stdout, stderr, error) => {
@@ -63,17 +124,16 @@ const self = module.exports = {
     },
 
     execModule: async (jobModule, funcName, ...funcArgs) => {
-        // support loop to execute sub-job of jobModule by setting process.env.DISABLE_LOOP = True
+        // default: process.env.DISABLE_SEEK = false, enable to execute deeper function by seeking funcArgs.
         const func = jobModule[funcName];
         if (func instanceof Function) {
-            let res = await func(...funcArgs);
-            return res;
+            return await func(...funcArgs);
         } else if (func !== undefined) {
-            if ((funcArgs.length > 0) && (func instanceof Object) && !self.config.DISABLE_LOOP) {
-                return self.execModule(func, ...funcArgs);
+            if ((funcArgs.length > 0) && (func instanceof Object) && !self.config.DISABLE_SEEK) {
+                return await self.execModule(func, ...funcArgs);
             } else {
-                self._log(`[$.${funcName}]=${func}`);  // funcName => Attribute Arguments
-                return func;
+                self._log(self.reprJobProperty(jobModule, funcName));  // funcName => Attribute Arguments
+                return await func;
             }
         } else {
             throw new Error(`$.${funcName}(${funcArgs}) is undefined`);
@@ -89,39 +149,31 @@ const self = module.exports = {
             console.log("require $descJobFuncs(<object: jobModule>)");
             process.exit(1);
         }
-        console.log(`[tips] ${jobModule.tips} \n`);
         if (jobModule.config) {
-            console.log(`[config]: ${self.stringify(jobModule.config, 2, 2)}\n`);
+            self._log(`[config]: ${self.stringify(jobModule.config, 2, 2)}\n`);
         }
         // ignore function's name startsWith "_"
         console.log("[Functions]: ");
-        for (let k in jobModule) {
+        for (let k of Object.getOwnPropertyNames(jobModule)) {
             if (!k.startsWith("_")) {
                 let pf = jobModule[k];
                 if (pf instanceof Function) {
-                    let desc = pf.toString().split("=>")[0];
-                    console.log(`  ${k.padEnd(32, " ")} : ${desc}`);
+                    let desc = self.reprFunction(pf);
+                    self._log(`  ${k.padEnd(32, " ")} : ${desc}`);
                 }
             }
         }
     },
 
-    descJobAttrs: (jobModule) => {
-        // ignore attribute's name startsWith "_"
-        jobModule = jobModule || ".";
-        if (typeof (jobModule) === "string") {
-            jobModule = require(jobModule);
-        }
-        if (typeof (jobModule) !== "object") {
-            console.log("require $descJobAttrs(<object: jobModule>)");
-            process.exit(1);
-        }
-        for (let k in jobModule) {
-            if (!k.startsWith("_")) {
-                let pf = jobModule[k];
-                if (!(pf instanceof Function)) {
-                    let desc = pf.toString().split("=>")[0];
-                    console.log(`  ${k.padEnd(32, " ")} : ${desc}`);
+    descJobAttrs: (jobModule, attr = "**") => {
+        // ignore the _attributes (prefix with "_")
+        let attrs = Object.keys(jobModule);
+        if (attrs.length > 0 && attr === "**") {
+            for (let k of attrs) {
+                if (!k.startsWith("_")) {
+                    let pf = jobModule[k];
+                    let pfv = self.parseValue(pf);
+                    self._log(`  ${k.padEnd(32, " ")} : ${pfv.repr}`);
                 }
             }
         }
@@ -136,12 +188,12 @@ const self = module.exports = {
                     return obj.toJSON();
                 }
                 let res = {};
-                for (let k in obj) {
+                for (let k of Object.keys(obj)) {
                     if (!k.startsWith("_")) {
                         let v = obj[k];
                         if (v instanceof Function) {
                             if (self.config.DEBUG) {
-                                res[k] = "$Funciton";
+                                res[k] = `$Function:: ${self.reprFunction(v)}`;
                             }
                         } else {
                             res[k] = self.stringify(v, depth - 1, indent_space);
@@ -157,7 +209,7 @@ const self = module.exports = {
 
     runIfMain: (filename, exports, exit_ok = true) => {
         if (process.mainModule.filename === filename) {
-            const args = process.argv.splice(2);
+            const args = process.argv.slice(2);
             if (args.length > 0) {
                 let t1 = new Date();
                 self.execModule(exports, ...args).then(res => {
@@ -169,16 +221,39 @@ const self = module.exports = {
                 }).catch(err => {
                     self._log(`[ERROR]: ${err.message}`);
                     self._log(`[EXEC_FILE]: ${filename}`);
+                    self._log(self.reprJobProperty(exports, "tips"));
                     self.descJobFuncs(exports);
                     process.exit(1);
                 });
             } else {
+                self._log(self.reprJobProperty(exports, "tips"));
                 self.descJobFuncs(exports);
                 process.exit(1);
             }
         }
     },
 
+    manageIfMain: (filename, jobs_index, exit_ok = true) => {
+        if (process.mainModule.filename === filename) {
+            const job = jobs_index[process.argv[2]] || jobs_index[`${process.argv[2]}.js`];
+            self._log(self.reprJobProperty(jobs_index, "tips"));
+            if (!job) {
+                self._log("[jobs]:");
+                for (let idx of Object.keys(jobs_index)) {
+                    let tk = jobs_index[idx];
+                    if (typeof (tk) === "object" && tk.tips) {
+                        self._log(self.reprJobProperty(tk, "tips"));
+                    } else {
+                        self._log(self.parseValue(tk).repr);
+                    }
+                }
+                process.exit(1);
+            } else {
+                self.runIfMain(filename, jobs_index, exit_ok);
+            }
+
+        }
+    },
 };
 
 self.runIfMain(__filename, self);
